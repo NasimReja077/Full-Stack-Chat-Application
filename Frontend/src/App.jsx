a8722a235a203dd3d6cf8d34a97f8d5e38d3c9a1
Nasim <nasim.reja.077@gmail.com>
2025-03-15 16:55:34 +0530

    Add 404 page and new pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from './Pages/Authentication page/LoginPage';
 import SignUpPage from './Pages/Authentication page/SignUpPage';
 import ForgotPasswordPage from './Pages/Authentication page/ForgotPasswordPage';
 import Loader from './components/Loder/Loader';
+import Error from './Pages/Error Pages/Error';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/signUp" element={<SignUpPage />} />
         <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
         <Route path="/loader" element={<Loader />} />
+        <Route path="/error" element={<Error />} />
       </Routes>
     </Router>
   )
